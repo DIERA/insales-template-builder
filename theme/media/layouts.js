52cbc56c324d5f72142e4a8b7ba750b07d578c56
Brainmurder <ivaninvv@mail.ru>
2017-07-10 15:08:09 +0300

    object-fit

--- theme/media/layouts.js
+++ theme/media/layouts.js
@@ -146,6 +146,31 @@ $(function () {
   };
 });
 
+$(function () {
+  /**
+   * Object-fit hack
+   */
+  var isSupports = typeof CSS != 'undefined' && CSS.supports && CSS.supports("object-fit", "cover");
+
+  if (!isSupports) {
+    $('.image-container.is-cover').each(function () {
+      var $container = $(this),
+        imgUrl = $container.find('img').prop('src');
+      if (imgUrl) {
+        $container
+          .css('backgroundImage', 'url(' + imgUrl + ')')
+          .addClass('with-trick');
+      }
+    });
+  }
+
+  $('body').addClass('dom-ready');
+
+  $(window).on('load', function(event) {
+    $('body').addClass('window-load');
+  });
+});
+
 $(document).ready(function() {
   Products.setConfig({
     initOption: true,
